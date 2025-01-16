c1b516082e12829da22eb4ae6deb5bb5ea087c7b
keerthi.sagar <keerthi.sagar@imr.ie>
2025-01-16 13:58:30 +0000

    Working ros1-ros2 bridge verion with laser scan tested

--- kmriiwa_navigation/launch/laserscan_merge.launch.py
+++ kmriiwa_navigation/launch/laserscan_merge.launch.py
@@ -40,12 +40,13 @@ def generate_launch_description():
                 '/', LaunchConfiguration('robot_name'), '/base/state/LaserB1Scan ',
                 '/', LaunchConfiguration('robot_name'), '/base/state/LaserB4Scan'
             ],
-            'angle_min': -135.0 * math.pi / 180.0,
-            'angle_max': 135.0 * math.pi / 180.0,
-            'angle_increment': 0.5 * math.pi / 180.0,
+            'angle_min': (-135.0 * 3.1415) / 180.0,
+            'angle_max': (135.0 * 3.1415) / 180.0,
+            'angle_increment': (0.5 * 3.1415) / 180.0,
             'scan_time': 0.5,
             'range_min': 0.12,
-            'range_max': 15.0
+            'range_max': 30.0
+            
         }]
     )
 
